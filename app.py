9b970ac4cbcaf3861aa049a60662ec5ac524cefd
ja669nle2bil <jakubil1221@mat.umk.pl>
2024-03-06 12:47:50 +0100

    pdf handler -alpha 😁

--- app.py
+++ app.py
@@ -4,7 +4,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "PDF to G-code Conversion Service"
+    return jsonify({"message": "PDF to G-code Conversion Service" })
+
+@app.route('/upload-podf', methods=['POST'])
+def upload_pdf():
+    # Sprawdzamy plik w żądaniu
+    if 'file' not in request.files:
+	return jsonify({"error": "No file part"}), 400
+
+    file = request.files['file']
+
+    # Nie wybrano pliku
+    if file.filename == '':
+	return jsonify({"error": "No selected file"}), 400
+    # obsluga wczyt. pdf
+    return jsonify({"success": "File uploaded successfully"})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
