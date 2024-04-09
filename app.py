9ca044dd86f5e03b42ee3de1ff0a51ea2c9d14a1
ja669nle2bil <jakubil1221@mat.umk.pl>
2024-04-09 23:54:38 +0200

    Validation error, wip

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ import fitz
 # # Tworzymy instancje dla aplikacji
 # app = create_app()
 
+app = Flask(__name__)
+# cors = CORS(app, origins='*')
+CORS(app, origins='*')
+
 pdf_blueprint = Blueprint('pdf_blueprint', __name__)
 
 @pdf_blueprint.route('/upload-pdf', methods=['POST'])
@@ -37,38 +41,68 @@ def upload_pdf():
     print(request.method)
     print("Inside upload_pdf route")
     print(request.files)  # Log the content of the files received
-    print(request.headers) # Log the request headers (Optional)
+    print(request.data)  # Print raw request data
+    print(request.form)  # Print form data
 
     if request.method == 'POST':
-        if 'file' not in request.files:
+        if 'file' in request.files:
+            file = request.files['file']
+            
+            print(f"Filename: {file.filename}")  # Log filename
+            print(f"File size: {len(file.read())}")  # Log file size
+            # print(request.files['file'].read(500))
+
+            if file.filename == '':
+                return jsonify({"error": "Nie wybrano żadnego pliku"}), 400
+            
+            if not allowed_file(file.filename):
+                return jsonify({"error": "Nieprawidlowy format pliku"}), 400
+            
+            # if file and allowed_file(file.filename):
+            try:
+                temp_pdf = tempfile.NamedTemporaryFile(delete=False, suffix='.pdf')
+                file.save(temp_pdf.name)
+
+                #Turned off for tests.
+                # pdf_data = temp_pdf.read()
+
+                # if not is_pdf_size_valid(temp_pdf.name):
+                #     os.remove(temp_pdf.name)
+                #     return jsonify({"error": "Rozmiar dokumentu PDF przekracza 20cm"}), 400
+                
+                # cache_key = temp_pdf.name
+                # cache.set(cache_key, pdf_data, timeout=3600)
+                temp_pdf.close()
+
+                print("PDF Path:", temp_pdf.name)  # Verify the temporary file path
+
+                return jsonify({"pdf_path": temp_pdf.name})
+                # else:
+                # return jsonify({"error": "Nieprawidłowy format pliku"}), 400
+            except Exception as e:
+                return jsonify({"error": f"Błąd przetwarzania pliku: {str(e)}"}), 500
+        else:
+            print("No file found in the request") 
             return jsonify({"error": "Brak części plikowej"}), 400
         
-        file = request.files['file']
-
-        if file.filename == '':
-            return jsonify({"error": "Nie wybrano żadnego pliku"}), 400
-        
-        if file and allowed_file(file.filename):
-            temp_pdf = tempfile.NamedTemporaryFile(delete=False, suffix='.pdf')
-            file.save(temp_pdf.name)
-
-            pdf_data = temp_pdf.read()
-
-            if not is_pdf_size_valid(temp_pdf.name):
-                os.remove(temp_pdf.name)
-                return jsonify({"error": "Rozmiar dokumentu PDF przekracza 20cm"}), 400
-            
-            cache_key = temp_pdf.name
-            cache.set(cache_key, pdf_data, timeout=3600)
-            temp_pdf.close()
-
-            return jsonify({"pdf_path": temp_pdf.name})
-        else:
-            return jsonify({"error": "Nieprawidłowy format pliku"}), 400
+    return jsonify({"message": "OK"})
         
 @pdf_blueprint.route('/upload-pdf', methods=['GET'])
 def redirect_from_get():
     return redirect('/')
+
+@pdf_blueprint.route('/validate-pdf', methods=['POST'])
+def validate_pdf():
+    if 'pdf_path' not in request.form:
+        return jsonify({"error": "No pdf_path provided for validartion"})
+    pdf_path = request.form['pdf_path']
+
+    if not is_pdf_size_valid(pdf_path):
+        return jsonify({"error": "PDF size exceeds limit"})
+
+    # Assuming your file saving in 'upload_pdf' works as intended
+    
+    return jsonify({"isValid": True})
 
 @pdf_blueprint.route('/workfile/<path:pdf_path>')
 def get_pdf(pdf_path):
@@ -99,12 +133,11 @@ def convert_pdf():
         if os.path.exists(output_svg_path):
             os.remove(output_svg_path)
 
-app = Flask(__name__)
+
 cache = Cache(app)
 app.config['CACHE_TYPE'] = 'simple'
 app.config['CACHE_DEFAULT_TIMEOUT'] = 3600
 app.register_blueprint(pdf_blueprint, url_prefix='/pdf')
-cors = CORS(app, origins='*')
 
 if __name__ == '__main__':
     app.run(debug=True) 
